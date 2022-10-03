28ba2924b70017920b4aa57fefbdd90b5998e32a
Rony Ventura <git@github.com:RVentura13/proyectoGit.git>
2022-10-03 12:56:11 -0600

    Estructura basica del proyecto creada con nextjs y tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio | Rony Ventura</title>
+        <meta name="description" content="Portafolio de proyectos de programación de Rony Ventura" />
+        <link rel="icon" href="/" />
+      </Head>
+      <h1 className="text-3xl font-bold underline bg-blue-500">
+      Hello world!
+    </h1>
+    </>
+  );
+};
+
+export default Home;
